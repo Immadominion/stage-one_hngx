5d85634257b040c0bb50a2ad8e9bc6ac6375df69
Imma Dominion <immadominion@gmail.com>
2023-09-07 16:34:06 +0100

    changed app icon to hng x... cool stuff

--- lib/src/navigation_controls.dart
+++ lib/src/navigation_controls.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stage_one/util/color_theme.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class NavigationControls extends StatelessWidget {
@@ -9,6 +11,8 @@ class NavigationControls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
+      color: immaBlack,
+      iconSize: 20.sp,
       icon: const Icon(Icons.replay),
       onPressed: () {
         controller.reload();
